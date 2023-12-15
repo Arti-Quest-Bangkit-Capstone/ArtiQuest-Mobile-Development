49a499fc624c75b2dc7785de53c79e7eb11b8539
rafidazahra <youremail@example.com>
2023-12-15 09:08:36 +0700

    edit

--- app/src/main/java/com/thequest/artiquest/view/profile/DetailProfileActivity.kt
+++ app/src/main/java/com/thequest/artiquest/view/profile/DetailProfileActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.thequest.artiquest.R
 
-class ProfileDetailActivity : AppCompatActivity() {
+class DetailProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_detail)
+        setContentView(R.layout.activity_detail_profile)
     }
 }
